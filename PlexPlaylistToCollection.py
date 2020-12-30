484f8ef7446e3e8fe4906c33c41aac1ff96bf10a
Daniel Rahn <danrahn@uw.edu>
2020-12-29 20:48:05 -0800

    Initial Commit

---
+++ PlexPlaylistToCollection.py
@@ -0,0 +1,339 @@
+import sys
+import os
+import argparse
+import requests
+import time
+import urllib
+import yaml
+import json
+
+class PlaylistToCollection:
+    def __init__(self):
+        self.get_config()
+
+
+    def get_config(self):
+        """Reads the config file from disk"""
+
+        self.valid = False
+        config_file = self.adjacent_file('config.yml')
+        config = None
+        if not os.path.exists(config_file):
+            print('WARN: Could not find config.yml! Make sure it\'s in the same directory as this script')
+        else:
+            with open(config_file) as f:
+                config = yaml.load(f, Loader=yaml.SafeLoader)
+        
+        if not config:
+            config = {}
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--host')
+        parser.add_argument('-t', '--token')
+        parser.add_argument('-p', '--playlist')
+        parser.add_argument('-s', '--section')
+        parser.add_argument('-c', '--collection')
+        self.cmd_args = parser.parse_args()
+        self.token = self.get_config_value(config, 'token')
+        self.host = self.get_config_value(config, 'host', 'http://localhost:32400')
+        self.section_id = self.get_config_value(config, 'section')
+        self.playlist_name = self.get_config_value(config, 'playlist')
+        self.collection_name = self.get_config_value(config, 'collection')
+        self.valid = True
+
+
+    def get_config_value(self, config, key, default=''):
+        """Returns a value from the given config, or the default value if not present"""
+
+        if key in config and config[key] != None:
+            if key in self.cmd_args:
+                value = self.cmd_args.__dict__[key]
+                if value != None:
+                    # Command-line args shadow config file
+                    print(f'WARN: Duplicate argument "{key}" found in both command-line arguments and config file. Using command-line value ("{self.cmd_args.__dict__[key]}")')
+                    return self.cmd_args.__dict__[key]
+            return config[key]
+
+        if len(default) != 0:
+            print(f'{key} not found in config or command line arguments, defaulting to "{default}"')
+
+        return default
+
+
+    def run(self):
+        """Kick off the processing"""
+
+        if not self.valid:
+            return
+
+        print()
+
+        if len(self.token) == 0:
+            self.token = input('Enter your Plex token: ')
+            print('Plex token set!\n')
+
+        playlist = self.find_playlist()
+        if not playlist:
+            print('Unable to find the right playlist, exiting...')
+            return
+
+        section = self.get_section()
+        if not section:
+            print('Unable to find the right library section, exiting...')
+            return
+
+        self.collection_name = self.get_collection_name(section)
+        if self.collection_name == None:
+            print('Unable to get the right collection, exiting...')
+            return
+
+        added = self.add_playlist_items_to_collection(section, playlist)
+        if not added:
+            print('Unable to add items to collection, exiting...')
+
+        print('\nDone!')
+
+
+    def find_playlist(self):
+        """
+        Checks whether the provided playlist exists and returns it.
+        If the playlist does not exist, or none was provided, ask the user to select one
+        """
+
+        playlists = self.get_json_response('/playlists', { 'playlistType' : 'video' })
+        if not playlists:
+            return None
+
+        matches = []
+        find = self.playlist_name
+        if len(find) != 0:
+            for playlist in playlists['Metadata']:
+                if playlist['title'].lower() == find.lower():
+                    matches.append(playlist)
+
+        count = len(matches)
+        if count == 1:
+            return matches[0]
+
+        if count == 0:
+            if len(find) == 0:
+                print('Available Playlists:\n')
+            else:
+                if not self.get_yes_no(f'Sorry, we could not find a playlist by the name of "{find}". List playlists'):
+                    return None
+
+            ret = self.select_playlist(playlists['Metadata'], lambda x: x['title'])
+            self.playlist_name = ret['title']
+            print(f'\nSelected {self.playlist_name}\n')
+            return ret
+
+        print('Multiple matching playlists found:\n')
+        display_function = lambda x: f'{x["title"]} ({x["leafCount"]} items, created {time.strftime("%Y-%m-%d", time.localtime(int(x["addedAt"])))})'
+        ret = self.select_playlist(matches, display_function)
+        self.playlist_name = ret['title']
+        print(f'\nSelected {self.playlist_name}\n')
+        return ret
+
+
+    def select_playlist(self, items, display_fn):
+        """Prompts the user to select a playlist to import from, optionally listing the items in a particular playlist"""
+        for i in range(len(items)):
+            print(f'[{i + 1}] {display_fn(items[i])}')
+
+        choice = input(f'\nSelect a playlist (-1 to cancel, prepend \'L\' to list the items in the playlist): ')
+        while not choice.isnumeric() or int(choice) < 1 or int(choice) > len(items):
+            if choice == '-1':
+                return None
+            elif choice[0].lower() == 'l':
+                choice = choice[1:]
+                if choice.isnumeric() and int(choice) > 0 and int(choice) <= len(items):
+                    self.print_playlist_items(items[int(choice) - 1])
+                    print()
+                    for i in range(len(items)):
+                        print(f'[{i + 1}] {display_fn(items[i])}')
+                    choice = input(f'\nSelect a playlist (-1 to cancel, prepend \'L\' to list the items in the playlist): ')
+            else:
+                choice = input('Invalid number, please try again (-1 to cancel): ')
+
+        return items[int(choice) - 1]
+
+
+    def print_playlist_items(self, playlist):
+        """Prints all items in the given playlist"""
+        print()
+        print(f'Items in "{playlist["title"]}":')
+        playlist_items = self.get_json_response(playlist['key'])
+        if not playlist_items:
+            print('\tSomething went wrong. Could not list playlist items\n')
+            return
+
+        if 'Metadata' not in playlist_items:
+            print('\tNo items found in playlist\n')
+            return
+
+        for item in playlist_items['Metadata']:
+            print(f'\t{item["title"]}')
+        print()
+
+
+    def get_section(self):
+        """Returns the section object that the collection will be added to"""
+        sections = self.get_json_response('/library/sections')
+        if not sections or 'Directory' not in sections:
+            return None
+
+        sections = sections['Directory']
+        find = self.section_id
+        if type(find) == int:
+            for section in sections:
+                if int(section['key']) == int(find):
+                    print(f'Found section {find}: "{section["title"]}"')
+                    return section
+
+            print(f'Provided library section {find} could not be found...\n')
+
+        print('Available Libraries:\n')
+        choices = {}
+        for section in sections:
+            print(f'[{section["key"]}] {section["title"]}')
+            choices[int(section['key'])] = section
+        print()
+
+        choice = input('Enter the library number (-1 to cancel): ')
+        while not choice.isnumeric() or int(choice) not in choices:
+            if choice == '-1':
+                return None
+            choice = input('Invalid section, please try again (-1 to cancel): ')
+
+        self.section_id = int(choice)
+        print(f'\nSelected "{choices[int(choice)]["title"]}"\n')
+        return choices[int(choice)]
+
+
+    def get_collection_name(self, section):
+        """Gets the collection name to add items to"""
+        collection_name = self.collection_name
+        if len(collection_name) == 0:
+            collection_name = input('Enter the collection name: ')
+        collections = self.get_json_response(f'/library/sections/{section["key"]}/collections')
+        if not collections:
+            return None
+
+        match = None
+        if collections['size'] != 0:
+            for collection in collections['Metadata']:
+                if collection_name.lower() == collection['title'].lower(): # Assume they meant to add to an existing collection if the names are equal, ignoring case
+                    match = collection
+                    collection_name = collection['title']
+                    break
+
+        confirm = False
+        print()
+        if match == None:
+            confirm = self.get_yes_no(f'Create a new collection "{collection_name}" and add all applicable items from your playlist to it')
+        else:
+            confirm = self.get_yes_no(f'"{collection_name}" already exists. Add applicable items from your playlist to it')
+
+        return collection_name if confirm else None
+
+    def add_playlist_items_to_collection(self, section, playlist):
+        """Adds all applicable items from the given playlist to the specificed collection"""
+        print(f'\nAdding {playlist["leafCount"]} items to collection "{self.collection_name}"\n')
+        playlist_items = self.get_json_response(playlist['key'])
+        if not playlist_items or 'Metadata' not in playlist_items:
+            return False
+        for item in playlist_items['Metadata']:
+            if int(item['librarySectionID']) != int(section['key']):
+                print(f'Not adding {item["title"]} to collection, as it is not in the library "{section["title"]}" ({item["librarySectionID"]} vs {section["key"]})')
+                continue
+            current_collections = self.get_item_collections(item)
+            if current_collections == None:
+                print(f'Error getting existing collections for "{item["title"]}", moving on...')
+                continue
+            if self.collection_name in current_collections:
+                print(f'{item["title"]} already exists in "{self.collection_name}"')
+                continue
+            if not self.add_item_to_collection(item, current_collections):
+                print(f'Unable to add "{item["title"]}" to "{self.collection_name}", moving on...')
+                continue
+
+            print(f'Added "{item["title"]}" to "{self.collection_name}"')
+
+        return True
+
+    def get_item_collections(self, item):
+        """Returns a list of collections that the given item belongs to"""
+        data = self.get_json_response(item['key'])
+        if not data:
+            return None
+
+        collections = []
+        if 'Metadata' not in data:
+            return collections
+        for metadata in data['Metadata']:
+            if 'Collection' not in metadata:
+                continue
+            for collection in metadata['Collection']:
+                collections.append(collection['tag'])
+        return collections
+
+
+    def add_item_to_collection(self, item, collections):
+        """Adds the given item to the new collection"""
+        key = item['key']
+        metadata_id = key[key.rfind('/') + 1:]
+        base = f'/library/sections/{self.section_id}/all'
+        type_to_int = { 'movie' : '1', 'show' : '2', 'season' : '3', 'episode' : '4' }
+        if item['type'] not in type_to_int:
+            return False
+        params = { 'type' : type_to_int[item['type']], 'id' : metadata_id }
+        for index in range(len(collections)):
+            params[f'collection%5B{index}%5D.tag.tag'] = collections[index]
+        params[f'collection%5B{len(collections)}%5D.tag.tag'] = self.collection_name
+        options = requests.options(self.url(base, params))
+        put = requests.put(self.url(base, params))
+        put.close()
+        options.close()
+        return True
+
+    def get_json_response(self, url, params={}):
+        """Returns the JSON response from the given URL"""
+        response = requests.get(self.url(url, params), headers={ 'Accept' : 'application/json' })
+        try:
+            data = json.loads(response.content)['MediaContainer']
+        except:
+            print('ERROR: Unexpected JSON response:\n')
+            print(response.content)
+            print()
+            data = None
+
+        response.close()
+        return data
+
+    def url(self, base, params={}):
+        """Builds and returns a url given a base and optional parameters. Parameter values are URL encoded"""
+        real_url = f'{self.host}{base}'
+        sep = '?'
+        for key, value in params.items():
+            real_url += f'{sep}{key}={urllib.parse.quote(value)}'
+            sep = '&'
+
+        return f'{real_url}{sep}X-Plex-Token={self.token}'
+
+    def get_yes_no(self, prompt):
+        """Prompt the user for a yes/no response, continuing to show the prompt until a value that starts with 'y' or 'n' is entered"""
+
+        while True:
+            response = input(f'{prompt} (y/n)? ')
+            ch = response.lower()[0] if len(response) > 0 else 'x'
+            if ch in ['y', 'n']:
+                return ch == 'y'
+
+    def adjacent_file(self, filename):
+        """Returns the file path for a file that is in the same directory as this script"""
+
+        return os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + os.sep + filename
+
+if __name__ == '__main__':
+    runner = PlaylistToCollection()
+    runner.run()
---
+++ README.md
@@ -0,0 +1,29 @@
+# Plex Playlist to Collection
+
+This script imports an existing playlist to a new or existing collection.
+
+## Usage
+
+`python PlexPlaylistFromKeywords.py [args]`
+
+## Requirements
+
+Requirements are outlined in requirements.txt, and can be installed via `pip install -r requirements.txt`
+
+## Configuration
+
+There are several required arguments, outlined below. If an argument is not passed in from the command line, it looks in the config file. If the argument is still not found, the script will interactively ask the user to provide the required values.
+
+### Arguments
+
+Value | Command line | Description
+---|---|---
+token | `-t`, `--token` | Your Plex token.
+host | `--host` | The host of the Plex server. Defaults to http://localhost:32400.
+section | `-s`, `--section` | The id of the library section to add the collection to.
+playlist | `-p`, `--playlist` | The name of the playlist to pull items from
+collection | `-c`, `--collection` | The name of the collection to create/add to
+
+## Issues
+
+It doesn't look like TV episodes can be added to collections, so best to stick with movie collections for now.
